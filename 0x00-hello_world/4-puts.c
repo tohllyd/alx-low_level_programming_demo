dc78e1ea6d3c83c705c7bea34d68c67778ba1465
Tolu <tohllyd@yahoo.com>
2023-06-15 19:59:07 -0700

    pputs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
