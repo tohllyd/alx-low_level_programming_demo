3837988e6f1a92ae9a9f3f536689dda2afccb8f5
Tolu <tohllyd@yahoo.com>
2023-06-15 20:51:35 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry into the code
+ * main - Entry into program
  *
- * Return (0)
+ * Return: 0
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
