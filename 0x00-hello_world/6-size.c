556728d154cf6a10b4a99cf2f26594d9fed3deeb
Tolu <tohllyd@yahoo.com>
2023-06-27 08:33:27 -0700

    type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int li;
 long long int ii;
 float f;
 
-printf("Size of a char: %zu byte(s) \n", sizeof(c));
-printf("Size of an int: %zu byte(s) \n", sizeof(d));
-printf("Size of a long int: %zu byte(s) \n", sizeof(li));
-printf("Size of a long long int: %zu byte(s) \n", sizeof(ii));
-printf("Size of a float: %zu byte(s) \n", sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(d));
+printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(ii));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
 
 return (0);
