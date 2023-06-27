7a7ffa8aef757e06c35c818b7dc32f338c4f8555
Tolu <tohllyd@yahoo.com>
2023-06-27 08:17:51 -0700

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+int d;
+long int li;
+long long int ii;
+float f;
+
+printf("Size of a char: %zu byte(s) \n", sizeof(c));
+printf("Size of an int: %zu byte(s) \n", sizeof(d));
+printf("Size of a long int: %zu byte(s) \n", sizeof(li));
+printf("Size of a long long int: %zu byte(s) \n", sizeof(ii));
+printf("Size of a float: %zu byte(s) \n", sizeof(f));
+
+
+return (0);
+}
