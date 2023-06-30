c6260adffa07eaeff674b6483271cb85b01f5e0f
Tolu <tohllyd@yahoo.com>
2023-06-29 22:15:01 -0700

    putchar alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point for our program
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
